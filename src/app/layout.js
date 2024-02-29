f658338702402af29b525c12694704d2989677f6
ismaelp96 <ismael6991@live.com>
2024-02-29 09:45:54 -0300

    Header Component

---
+++ src/app/layout.js
@@ -0,0 +1,18 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Google Clone',
+  description:
+    'An open source google clone built with next.js and Tailwind CSS.',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
