c1f7ff0778c191fa270f8608432880c899a9e604
ismaelp96 <ismael6991@live.com>
2024-02-29 17:07:04 -0300

    Complete the Search Header Options

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import SearchBox from './SearchBox';
 import { RiSettings3Line } from 'react-icons/ri';
 import { TbGridDots } from 'react-icons/tb';
+import SearchHeaderOptions from './SearchHeaderOptions';
 
 export default function SearchHeader() {
   return (
@@ -13,8 +14,8 @@ export default function SearchHeader() {
           <Image
             src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png'
             alt='Google Logo'
-            width={300}
-            height={100}
+            width={120}
+            height={40}
             style={{ width: 'auto' }}
             priority
           />
@@ -30,6 +31,7 @@ export default function SearchHeader() {
           Sign In
         </button>
       </div>
+      <SearchHeaderOptions />
     </header>
   );
 }
