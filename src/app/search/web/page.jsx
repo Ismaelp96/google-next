12865d7df7d78e2675eb09c6f10b317b329799bd
ismaelp96 <ismael6991@live.com>
2024-03-01 11:38:02 -0300

    Fetch data from Google Search API and show the titles

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,9 @@
-export default function WebSearchPage() {
-  return <div>Web Search Page</div>;
+export default async function WebSearchPage({ searchParams }) {
+  const response =
+    await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}
+  `);
+  const data = await response.json();
+  const results = data.items;
+  console.log(data);
+  return <div>{results && results.map((result) => <p>{result.title}</p>)}</div>;
 }
--- src/components/SearchHeaderOptions.jsx
+++ src/components/SearchHeaderOptions.jsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { AiOutlineCamera, AiOutlineSearch } from 'react-icons/ai';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { AiOutlineCamera, AiOutlineSearch } from 'react-icons/ai';
 
 export default function SearchHeaderOptions() {
   const router = useRouter();
@@ -18,19 +18,19 @@ export default function SearchHeaderOptions() {
   return (
     <div className='flex space-x-2 select-none border-b w-full justify-center lg:justify-start lg:pl-52 text-gray-700 text-sm'>
       <div
+        onClick={() => selectTab('All')}
         className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${
           pathname === '/search/web' && '!text-blue-600 !border-blue-600'
         }`}
-        onClick={() => selectTab('All')}
       >
         <AiOutlineSearch className='text-md' />
         <p>All</p>
       </div>
       <div
+        onClick={() => selectTab('Images')}
         className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${
           pathname === '/search/image' && '!text-blue-600 !border-blue-600'
         }`}
-        onClick={() => selectTab('Images')}
       >
         <AiOutlineCamera className='text-md' />
         <p>Images</p>
